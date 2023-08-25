3b09e46aa3caa3e9d420020e796976241f71ee45
Pugnatorfn <92223908+Pugnatorfn@users.noreply.github.com>
2023-08-25 09:12:17 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-analytics.js";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -15,9 +16,9 @@ const firebaseConfig = {
   measurementId: "G-460S024LHB"
 };
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const itemInput = document.getElementById('item');
 const expirationInput = document.getElementById('expiration');
